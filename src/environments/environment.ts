4d0177101d0cd7ca3291c59a56b05283fc4f00fa
Mohamed Galal <58759061+mgalal8866@users.noreply.github.com>
2025-08-10 03:00:21 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://hoksowg48wcgco0gg44wo40c.209.74.80.181.sslip.io/api/v1'
+    baseUrl: 'https://admin.alyusr.edxcode.com/api/v1'
 };
